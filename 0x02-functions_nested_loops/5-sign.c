a468f84d4a9e7d1d85ae5deed441078e2123ab12
selinakanu <selinakanu16@gmail.com>
2022-03-16 09:58:26 -0700

    nested loop to execute signs in alphabets

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -27,33 +27,3 @@ return (-1);
 }
 
 }
-gibson uwhetu5:50 PM
-#include "main.h"
-#include <stdio.h>
-
-/**
- * _abs - value absolute
- *
- *@n: The int to print
- * Return: Always 0.
- */
-
-int _abs(int n)
-{
-
-if (n < 0)
-{
-return (n * (-1));
-}
-
-else if (n == 0)
-{
-return (0);
-}
-
-else
-{
-return (n);
-}
-
-}
