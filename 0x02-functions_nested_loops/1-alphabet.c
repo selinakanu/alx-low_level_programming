1a1c6ebca012fc1f5bc4f1ad0d4464fb8d5f062e
selinakanu <selinakanu16@gmail.com>
2022-03-16 08:56:35 -0700

    my nested loop to execute alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,22 @@
-#include <unistd.h>
+r(alphabet);
+}
+
+_putchar('\n');
+
+}#include "main.h"
 
 /**
- * _putchar - writes the character c to stout
- * @c: The character to print
+ * print_alphabet - print alphabet lowercase
  *
- * Return on sucess: 1
- * On error, -1 is returned, and errno is set appropriately
+ * Return: Always 0.
  */
-int _putchar(char c)
+
+void print_alphabet(void)
 {
-	return (write(1, &c, 1));
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putcha('\n');
+
 }
